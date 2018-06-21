3148eaae6613b85c7d36f35cddc89fa5748d1e3c
dadopiz <davide.pizzato87@gmail.com>
2018-06-21 14:24:38 +0200

    Implemented M3 class.

--- lib/include/enigma/m3.h
+++ lib/include/enigma/m3.h
@@ -1,15 +1,61 @@
 #ifndef ENIGMA_M3_H
 #define ENIGMA_M3_H
 
+#include <cassert>
+
 #include <enigma/global.h>
+#include <enigma/rotor.h>
+#include <enigma/reflector.h>
 
 namespace enigma {
 
-class M3 {
+class ENIGMA_API M3 {
 public:
-    M3() = default;
+    M3(const Rotor& fast_rotor
+       , const Rotor& midd_rotor
+       , const Rotor& slow_rotor
+       , const Reflector& reflector)
+        : fast_rotor_(fast_rotor)
+        , midd_rotor_(midd_rotor)
+        , slow_rotor_(slow_rotor)
+        , reflector_(reflector)
+    {}
+
+    char Translate(char letter) {
+        assert(is_valid(letter) && "invalid letter.");
+
+        Turn();
+
+        letter = fast_rotor_.TranslateStraight(letter);
+        letter = midd_rotor_.TranslateStraight(letter);
+        letter = slow_rotor_.TranslateStraight(letter);
+
+        letter = reflector_.Translate(letter);
+
+        letter = slow_rotor_.TranslateReverse(letter);
+        letter = midd_rotor_.TranslateReverse(letter);
+        letter = fast_rotor_.TranslateReverse(letter);
+
+        return letter;
+    }
+
+private:
+    void Turn() {
+        if(midd_rotor_.IsNotch())
+            slow_rotor_.Turn();
+
+        if(fast_rotor_.IsNotch() || midd_rotor_.IsNotch())
+            midd_rotor_.Turn();
+
+        fast_rotor_.Turn();
+    }
+
+    Rotor fast_rotor_;
+    Rotor midd_rotor_;
+    Rotor slow_rotor_;
+    Reflector reflector_;
 };
 
 }
 
-#endif
+#endif // ENIGMA_M3_H
