8c96a7975ea0dd8e956e7199cdc812c9d7807944
dadopiz <davide.pizzato87@gmail.com>
2018-06-29 14:28:12 +0200

    Completed tests for M3.

--- tests/m3_test.cpp
+++ tests/m3_test.cpp
@@ -24,3 +24,43 @@ TEST(m3_test, test_translate_phrase) {
     std::string result("BDZGOWCXLTKSBTMCDLPBMUQOFXYHCXTGYJFLINHNXSHIUNTHEORXPQPKOVHC");
     EXPECT_EQ(result, machine.Translate(input));
 }
+
+TEST(m3_test, test_connect) {
+    using enigma::historical::rotor;
+    using enigma::historical::reflector;
+    using enigma::historical::M3;
+
+    M3 machine(rotor::III, rotor::II, rotor::I, reflector::B);
+
+    EXPECT_TRUE(machine.Connect('A', 'Z'));
+
+    EXPECT_EQ('U', machine.Translate('A'));
+
+    machine.ResetPlugboard();
+
+    EXPECT_EQ('D', machine.Translate('A'));
+}
+
+TEST(m3_test, test_settings) {
+    using enigma::historical::rotor;
+    using enigma::historical::reflector;
+    using enigma::historical::M3;
+
+    M3 machine(rotor::III, rotor::II, rotor::I, reflector::B);
+
+    EXPECT_EQ('B', machine.Translate('A'));
+
+    machine.GrundStellung('A', 'A', 'A');
+
+    EXPECT_EQ('B', machine.Translate('A'));
+
+    machine.GrundStellung('A', 'A', 'A');
+    machine.RingStellung('B', 'B', 'B');
+
+    EXPECT_EQ('E', machine.Translate('A'));
+
+    machine.GrundStellung('A', 'A', 'A');
+    machine.RingStellung('A', 'A', 'A');
+
+    EXPECT_EQ('B', machine.Translate('A'));
+}
